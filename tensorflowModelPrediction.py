69db41603810ce8b4e8bf068424aa14d70a8e128
Tiffany <ntruong30@horizon.csueastbay.edu>
2021-09-14 20:18:09 -0700

    Included a print for test accuracy.

--- tensorflowModelPrediction.py
+++ tensorflowModelPrediction.py
@@ -1,5 +1,6 @@
 """project: creating a model that predicts the accuracy of images using
-machine learning (tensorflow). this project uses tensorflow's sample data set."""
+machine learning (tensorflow). this project uses tensorflow's sample data set/
+tensorflow keras, as high level API to build and train models in tensorflow."""
 
 import tensorflow as tf
 from tensorflow import keras
@@ -16,7 +17,8 @@ for machine learning/neural networks, you don't want to pass in all the data whe
 you train. you want to pass in 80%ish of your data into the network to train and
 then test the rest of the data for accuracy"""
 
-#can load our data like this thanks to keras
+#'train' = arrays for the training set, the data that the model uses
+#'test' = the data that the model is tested against
 (train_images, train_labels), (test_images, test_labels) = data.load_data()
 
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
@@ -43,6 +45,9 @@ model.fit(train_images, train_labels, epochs=5) #epoch - decides how many times 
 
 #train the model
 prediction = model.predict(test_images) #gives predictions by passing in a bunch of items
+test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+
+print('\nTest accuracy:', test_acc)
 
 #get the largest value and find the index, returning the highest predicted percentage
 #get the name of the item by passing it into class_names
