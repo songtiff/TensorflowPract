87063a84a19f030a82849456181bbdac5bf923ad
Tiffany <ntruong30@horizon.csueastbay.edu>
2021-09-17 17:27:32 -0700

    Added image prediction vs actual results with picture and graph bar accuracy.

--- tensorflowModelPrediction.py
+++ tensorflowModelPrediction.py
@@ -52,12 +52,57 @@ print('\nTest accuracy:', test_acc)
 #get the largest value and find the index, returning the highest predicted percentage
 #get the name of the item by passing it into class_names
 #this sets up a way to see the images of what it actually is vs the prediction 
-for i in range(5):
+"""for i in range(5):
     plt.grid(False)
     plt.imshow(test_images[i], cmap=plt.cm.binary)
     plt.xlabel("Actual: " + class_names[test_labels[i]]) 
     plt.title("Prediction" + class_names[np.argmax(prediction[i])])
-    plt.show()
+    plt.show()"""
+
+#graph to look at the full set of 10 class predictions
+def plot_image(i, predictions_array, true_label, img):
+    true_label, img = true_label[i], img[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+
+    plt.imshow(img, cmap=plt.cm.binary)
+    predicted_label = np.argmax(predictions_array)
+    if predicted_label == true_label:
+        color = 'blue'
+    else:
+        color = 'red'
+
+    plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                100*np.max(predictions_array),
+                                class_names[true_label]),
+                                color=color)
+                                            
+def plot_value_array(i, predictions_array, true_label):
+    true_label = true_label[i]
+    plt.grid(False)
+    plt.xticks(range(10))
+    plt.yticks([])
+    thisplot = plt.bar(range(10), predictions_array, color="#777777")
+    plt.ylim([0, 1])
+    predicted_label = np.argmax(predictions_array)
+
+    thisplot[predicted_label].set_color('red')
+    thisplot[true_label].set_color('blue')
+
+#verifying the predictions of several images
+num_rows = 5
+num_cols = 3
+num_images = num_rows * num_cols
+
+plt.figure(figsize=(2*2*num_cols, 2*num_rows))
+for i in range(num_images):
+    plt.subplot(num_rows, 2*num_cols, 2*i+1)
+    plot_image(i, prediction[i], test_labels, test_images)
+    plt.subplot(num_rows, 2*num_cols, 2*i+2)
+    plot_value_array(i, prediction[i], test_labels)
+plt.tight_layout()
+plt.show()
 
 
 
