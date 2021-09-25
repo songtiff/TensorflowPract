e6ee7e0389fdb815866e473650dca72136348b57
Tiffany <ntruong30@horizon.csueastbay.edu>
2021-09-24 19:12:47 -0700

    Inference and export new data

--- textClassificationExercise.py
+++ textClassificationExercise.py
@@ -149,3 +149,44 @@ print("Loss: ", loss)
 print("Accuracy: ", accuracy)
 
 #plot of accuracy and loss over time
+history_dict = history.history
+history_dict.keys()
+
+acc = history_dict['binary_accuracy']
+val_acc = history_dict['val_binary_accuracy']
+loss = history_dict['loss']
+val_loss = history_dict['val_loss']
+
+epochs = range(1, len(acc) + 1)
+
+#taining and validation loss
+plt.plot(epochs, loss,'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
+#training and validation accuracy
+plt.plot(epochs, acc, 'bo', label='Training accuracy')
+plt.plot(epochs, val_acc, 'b', label='Validation accuracy')
+plt.title('Training and validation accruacy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend(loc='lower right')
+plt.show()
+
+#export model
+export_model = tf.keras.Sequential([
+    vectorize_layer,
+    model,
+    layers.Activation('sigmoid')
+])
+
+export_model.compile(
+    loss = losses.BinaryCrossentropy(from_logits=False), optimizer="adam", metrics=['accuracy']
+)
+
+loss, accuracy = export_model.evaluate(raw_test_ds)
+print(accuracy)
