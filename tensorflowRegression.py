a8a48fe051dd9178a5a5536c7e4b7b83fd3a9386
Tiffany <ntruong30@horizon.csueastbay.edu>
2021-11-01 20:14:59 -0700

    Normalized and cleaned data.

--- tensorflowRegression.py
+++ tensorflowRegression.py
@@ -32,3 +32,37 @@ dataset = dataset.dropna()
 dataset['Origin'] = dataset['Origin'].map({1: 'USA', 2: 'Europe', 3: 'Japan'})
 dataset = pd.get_dummies(dataset, columns=['Origin'], prefix='', prefix_sep='')
 print(dataset.tail)
+
+#split data into training and test sets
+train_dataset = dataset.sample(frac=0.8, random_state=0)
+test_dataset = dataset.drop(train_dataset.index) #used at final evaluation 
+
+#inspect data
+print(sns.pairplot(train_dataset[['MPG', 'Cylinders', 'Displacement', 'Weight']], diag_kind='kde'))
+print(train_dataset.describe().transpose())
+
+#separate target value aka 'label' from features- label is the value that
+#you will train the model to predict
+
+train_features = train_dataset.copy()
+test_features = test_dataset.copy()
+
+train_labels = train_features.pop('MPG')
+test_labels = test_features.pop('MPG')
+
+#normalize data
+print(train_dataset.describe().transpose()[['mean', 'std']])
+
+#add feature normalization 
+normalizer = tf.keras.layers.Normalization(axis=-1)
+normalizer.adapt(np.array(train_features))
+
+#calculate mean and variance and store it in a layer
+print(normalizer.mean.numpy())
+
+first = np.array(train_features[:1])
+
+with np.printoptions(precision=2, suppress=True):
+    print('First example: ', first)
+    print()
+    print('Normalized: ', normalizer(first).numpy())
